f6e95ca68a8bb2a182e571cc2a7768c2ae118391
QaziH <qazirashid@gmail.com>
2014-05-16 01:46:21 +0100

    Made the code more readable

--- euclid/euclid.c
+++ euclid/euclid.c
@@ -1,10 +1,12 @@
+// @Author: Qazi
+// This is an algorithm for computing GCD of two positive integers m and n using Euclid's algorithm.
 #include <stdio.h>
 #include <math.h>
 int main(int argc, char ** argv){
-  // This is an algorithm for computing GCD of two positive integers m and n using Euclid's algorithm.
+
   int m = 0, n = 0, r = 0; // m and n are positive integers, r will be remainder 
   if(argc !=3){
-    printf("This algorithm requires two positive numbers. usage: euclid n m ");
+    fprintf(stderr,"This algorithm requires two positive numbers. usage example: `gcd 32 12`\n");
     return(1); // program terminated without success
   }
   sscanf(argv[1], "%d", &m); // convert first argument from string to int
@@ -12,21 +14,14 @@ int main(int argc, char ** argv){
   
   // test for positivity
   if( n <=0 || m <= 0){
-    printf("Both integers must be positive: The program will terminate without success");
-      return(1);
+    fprintf(stderr,"Both integers must be positive: The program will terminate without success\n");
+    return(1);
   }
-  
-  if(n>m){ //if n is the larger integer, swap them. Make m the larger int
-    r = m;
-    m = n;
-    n = r;
-    r = 1; //reset r after swap
-  }
-  // now m contains larger int and n contains smaller int. We can start the Euclid Algorithm
+  // We can start the Euclid Algorithm now.
   step1: r = m%n;  // Divide m by n, store remainder in r.
   if(r == 0){
     printf(" The GCD is %d\n",n);
-    return(0); // algorithm terminated successfully
+    return(0); // algorithm terminated with success
   }
   else{
     printf("m = %d, n = %d, r = %d : Starting new iteration\n",m,n,r);
